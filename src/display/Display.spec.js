4d693540a4eeb799fabf5f25f91c01fb546a625f
Aman <amsingh714@gmail.com>
2019-04-10 15:48:18 -0400

    finished display tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,35 @@
-// Test away!
+import React from "react";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("should render Display", () => {
+    render(<Display />);
+  });
+
+  it("should display open", () => {
+    const { getByText } = render(<Display closed={false} />);
+
+    getByText("Open");
+  });
+
+  it("should display unlocked", () => {
+    const { getByText } = render(<Display locked={false} />);
+
+    getByText("Unlocked");
+  });
+
+  it("should display closed", () => {
+    const { getByText } = render(<Display closed={true} />);
+
+    getByText("Closed");
+  });
+
+  it("should display locked", () => {
+    const { getByText } = render(<Display locked={true} />);
+
+    getByText("Locked");
+  });
+});
